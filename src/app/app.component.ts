64489ec3f8d733023fdf5e89ed2745b8725592f4
Charly1590 <carlos19.pe@gmail.com>
2021-08-13 21:29:53 -0500

    cambio de iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Registro', url: '/registro', icon: 'mail' },
-    { title: 'Inscripciones', url: '/inscripcion', icon: 'paper-plane' },
-    { title: 'Llegada', url: '/llegada', icon: 'heart' },
-    { title: 'Resultados', url: '/resultados', icon: 'archive' },
+    { title: 'Registro', url: '/registro', icon: 'list' },
+    { title: 'Inscripciones', url: '/inscripcion', icon: 'person-add' },
+    { title: 'Llegada', url: '/llegada', icon: 'speedometer' },
+    { title: 'Resultados', url: '/resultados', icon: 'ribbon' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
